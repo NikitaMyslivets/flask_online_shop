693ed761aaa5eefe484068f2ee926a661d72487a
Nikita <nikita.myslivets@mail.ru>
2021-01-12 00:37:41 +0300

    show database element on main page

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 # database
@@ -14,9 +15,14 @@ class Item(db.Model):
     price = db.Column(db.Integer, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
 
+    def __repr__(self):
+        return self.title
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
+
 
 
 @app.route('/about')
@@ -24,8 +30,20 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/create')
+@app.route('/create', methods=['POST', 'GET'])
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error'
     return render_template('create.html')
 
 
