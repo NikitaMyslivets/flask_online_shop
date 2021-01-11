7afda089ef562aa92f2e161799d646d15f8af188
Nikita <nikita.myslivets@mail.ru>
2021-01-11 23:55:23 +0300

    create db and form for add product

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_shop.db'
+# settings with database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Item(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
 
 @app.route('/')
 def index():
@@ -11,6 +22,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
